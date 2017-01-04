32c8813ede597c44e20edb7e43b6ef324a416ebe
Yvan Pearson <ypearson@nklabs.com>
2017-01-04 18:55:53 -0500

    test sub 1 sec.

--- main.c
+++ main.c
@@ -21,14 +21,16 @@ int timerfd_gettime(int fd, struct itimerspec *curr_value);
 int main(int argc, char const *argv[])
 {
 
+    char v[8];
+
     struct timespec it_interval = {
-                            .tv_sec = 1,
-                            .tv_nsec = 0,
+                            .tv_sec = 0,
+                            .tv_nsec = 900000000,
                             };
 
     struct timespec it_value = {
-                            .tv_sec = 1,
-                            .tv_nsec = 0,
+                            .tv_sec = 0,
+                            .tv_nsec = 900000000,
                             };
 
     const struct itimerspec new_value = {
@@ -36,55 +38,31 @@ int main(int argc, char const *argv[])
                                     .it_value = it_value,
                                   };
 
-    struct itimerspec old_value = {
-                                    .it_interval = it_interval,
-                                    .it_value = it_value,
-                                  };
-
     int timerfd = timerfd_create(CLOCK_MONOTONIC, 0);
     int ret = timerfd_settime(timerfd, 0, & new_value, 0);
     printf("ret = %d\n", ret);
-    char v[8];
-    read(timerfd, v, 8);
-
-
-    // for(;;)
-    // {
-    //     read(timerfd, v, 8);
-    //     printf("%s\n","timeout\n" );
-    // }
-
-
 
     int efd = epoll_create(1);
     struct epoll_event ev, events[1];
     ev.events = EPOLLIN;
     ev.data.fd = timerfd;
-    epoll_ctl(efd, EPOLL_CTL_ADD, timerfd, &ev);
 
+    epoll_ctl(efd, EPOLL_CTL_ADD, timerfd, &ev);
+    int i = 0;
     while(1)
     {
-
         epoll_wait(efd, events, 1, -1);
         read(timerfd, v, 8);
 
-        printf("%s\n", "some event...\n");
-
         if(events[0].data.fd == timerfd)
         {
-            printf("%s\n", ".");
+            printf("%d ", i++);
+            fflush(stdout);
         }
-
+        else
+        {
+            printf("%s\n", "X");
+        }
     }
-
-
-
-
-
-
-
-
-
-
     return 0;
 }
